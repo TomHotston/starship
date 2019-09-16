22d989f6bd4b08a4f27076061b598ab6fb0b5d4d
Nick Young <nick@nickwb.net>
2019-09-16 18:21:09 +1000

    WIP: Smarter dotnet module

--- src/modules/dotnet.rs
+++ src/modules/dotnet.rs
@@ -1,6 +1,9 @@
 use ansi_term::Color;
+use std::path::Path;
 use std::process::Command;
 use std::str;
+
+use std::ffi::OsStr;
 
 use super::{Context, Module};
 
@@ -13,12 +16,9 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
     let mut module = context.new_module("dotnet");
 
-    let is_dotnet = context
-        .try_begin_scan()?
-        .set_extensions(&["sln", "csproj", "fsproj", "xproj"])
-        .is_match();
+    let dotnet_files = get_local_files_filtered(context).ok()?;
 
-    if !is_dotnet {
+    if dotnet_files.len() == 0 {
         return None;
     }
     let version = get_dotnet_version()?;
@@ -27,6 +27,57 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
     module.new_segment("version", &version);
 
     Some(module)
+}
+
+struct DotNetFile<'a> {
+    path: &'a Path,
+    file_type: FileType,
+}
+
+enum FileType {
+    ProjectJson,
+    ProjectFile,
+    GlobalJson,
+    SolutionFile,
+}
+
+fn get_local_files_filtered<'a>(
+    context: &'a Context,
+) -> Result<Vec<DotNetFile<'a>>, std::io::Error> {
+    Ok(context
+        .get_dir_files()?
+        .iter()
+        .filter_map(|p| {
+            is_dotnet_relevant_file(p).map(|t| DotNetFile {
+                path: p.as_ref(),
+                file_type: t,
+            })
+        })
+        .collect())
+}
+
+fn is_dotnet_relevant_file(path: &Path) -> Option<FileType> {
+    let file_name_lower = map_to_lower(path.file_name());
+
+    match file_name_lower.as_ref().map(|f| f.as_ref()) {
+        Some("global.json") => return Some(FileType::GlobalJson),
+        Some("project.json") => return Some(FileType::ProjectJson),
+        _ => (),
+    };
+
+    let extension_lower = map_to_lower(path.extension());
+
+    match file_name_lower.as_ref().map(|f| f.as_ref()) {
+        Some("sln") => return Some(FileType::SolutionFile),
+        Some("csproj") | Some("fsproj") | Some("xproj") => return Some(FileType::ProjectFile),
+        _ => (),
+    };
+
+    None
+}
+
+fn map_to_lower(value: Option<&OsStr>) -> Option<String> {
+    Some(value?.to_str()?.to_ascii_lowercase())
 }
 
 fn get_dotnet_version() -> Option<String> {
